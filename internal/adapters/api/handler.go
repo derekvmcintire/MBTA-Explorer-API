671814290ed801e36b1fd694d4b2a0a7cc3bdc52
derekvmcintire <derekcommonground@gmail.com>
2024-12-28 22:32:29 -0500

    add comments

--- internal/adapters/api/handler.go
+++ internal/adapters/api/handler.go
@@ -7,18 +7,27 @@ import (
 	"net/http"                   // Import the net/http package to handle HTTP requests and responses
 )
 
+// FetchShapes is an HTTP handler function that returns the shape data for a given route.
+// It extracts the route ID from the request query parameters and calls the FetchData service to retrieve shape data.
 func FetchShapes(fetchData usecases.FetchData) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		// Extract the route ID from the query parameters of the URL (e.g., /shapes?route_id=Red)
 		routeID := r.URL.Query().Get("route_id")
+
+		// Call the GetShapes method of the fetchData service to get the shapes for the given route ID
 		shapes, err := fetchData.GetShapes(routeID)
 
+		// If an error occurred while fetching the shapes, log the error and return a 500 Internal Server Error
 		if err != nil {
-			log.Println("got sum err in FetchShapes: ", err)
+			log.Println("Error in FetchShapes:", err)
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
+		// Set the response header to specify that the content being returned is in JSON format
 		w.Header().Set("Content-Type", "application/json")
+
+		// Encode the shapes data as JSON and send it in the response body
 		json.NewEncoder(w).Encode(shapes)
 	}
 }
@@ -35,7 +44,7 @@ func FetchStops(fetchData usecases.FetchData) http.HandlerFunc {
 
 		// If an error occurred while fetching the stops, log the error and return a 500 Internal Server Error
 		if err != nil {
-			log.Println("got sum err in FetchStops: ", err)
+			log.Println("Error in FetchStops:", err)
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
@@ -58,8 +67,9 @@ func UpdateLiveData(fetchData usecases.FetchData) http.HandlerFunc {
 		// Call the GetLiveData method of the fetchData service to get the live data for the given route ID
 		vehicles, err := fetchData.GetLiveData(routeID)
 
-		// If an error occurred while fetching the live data, return a 500 Internal Server Error
+		// If an error occurred while fetching the live data, log the error and return a 500 Internal Server Error
 		if err != nil {
+			log.Println("Error in UpdateLiveData:", err)
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
