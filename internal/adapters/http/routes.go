43a842210d8364305411a1940a501c98ce263186
Derek McIntire <77245762+derekvmcintire@users.noreply.github.com>
2025-01-12 18:46:54 -0500

    updated vehicle endpoint (#21)

--- internal/adapters/http/routes.go
+++ internal/adapters/http/routes.go
@@ -18,16 +18,12 @@ import (
 func RegisterRoutes(router *mux.Router, mbtaApiHelper usecases.MbtaApiHelper, sm ports.StreamManager) {
 
 	// Initialize handlers for each route
-	streamHandler := handlers.NewStreamVehiclesHandler(sm)                                        // Handles streaming of vehicle data
-	sotpsHandler := middleware.CompressHandler(handlers.FetchRouteStops(mbtaApiHelper))           // Handles fetching route stops
-	shapesHandler := middleware.CompressHandler(handlers.FetchRouteShapes(mbtaApiHelper))         // Handles fetching route shapes
-	livePositionHandler := middleware.CompressHandler(handlers.UpdateLivePosition(mbtaApiHelper)) // Handles live vehicle positions
-	routesHandler := middleware.CompressHandler(handlers.FetchRoutes(mbtaApiHelper))
+	streamVehiclesHandler := handlers.NewStreamVehiclesHandler(sm)                                       // Handles streaming of vehicle data
+	vehiclePositionHandler := middleware.CompressHandler(handlers.VehiclePositionHandler(mbtaApiHelper)) // Handles live vehicle positions
+	routesHandler := middleware.CompressHandler(handlers.RouteHandler(mbtaApiHelper))
 
 	// Define HTTP endpoints and their corresponding handlers
-	router.Handle("/stream/vehicles", streamHandler)               // Streaming endpoint for vehicle data
-	router.Handle("/api/stops", sotpsHandler).Methods("GET")       // Fetch route stops via GET
-	router.Handle("/api/shapes", shapesHandler).Methods("GET")     // Fetch route shapes via GET
-	router.Handle("/api/routes", routesHandler).Methods("GET")     // Fetch route list via GET
-	router.Handle("/api/live", livePositionHandler).Methods("GET") // Fetch live vehicle positions via GET
+	router.Handle("/stream/vehicles", streamVehiclesHandler)              // Streaming endpoint for vehicle data
+	router.Handle("/api/routes", routesHandler).Methods("GET")            // Fetch route list via GET
+	router.Handle("/api/vehicles", vehiclePositionHandler).Methods("GET") // Fetch live vehicle positions via GET
 }
